bf3e3c67ac7461d62b9e9798753075d43435f760
RandimaD25 <randimamethminid525@gmail.com>
2025-08-12 18:02:56 +0530

    feat: input and output method

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.ReadKey();
 
             //Implicit type conversion
             int number = 100;
@@ -20,9 +19,41 @@ namespace ConsoleApp1
             //Explicit type conversion
             double price = 99.99;
             int roundedPrice = (int)price;
+            String value = roundedPrice.ToString();
 
             Console.WriteLine(roundedPrice);
             Console.ReadKey();
+
+            //Constants
+            const double TEXT_RATE = 0.05;
+
+            //Var keyword
+            var name = "Randima";
+
+            //Input and Output Methods
+
+            //Basic printing
+            Console.WriteLine("Hello, World!");
+
+            //Printing variables
+            string fName = "Laura";
+            Console.WriteLine(fName);
+
+            //Write vs WriteLine
+            Console.Write("Hello"); // No newline
+
+            //Print empty line
+            Console.WriteLine();
+
+            //ReadLine Method
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();
+
+            //Getting numbers required conversion
+            Console.Write("Enter your age: ");
+            int userAge = int.Parse(Console.ReadLine());
+
+
 
         }
     }
